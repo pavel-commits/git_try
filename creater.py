bccd04d35b8d80a5d2e55e120cef4f23e3d0dc32
pavel <pavelkocozlov@yandex.ru>
2025-02-22 13:26:42 +0300

    first commit

---
+++ creater.py
@@ -0,0 +1,3 @@
+def write_text(file, text):
+    with open(file, "w") as f:
+        f.write(text)
---
+++ main.py
@@ -0,0 +1,19 @@
+from creater import write_text
+
+s = """asodfhvaosijdfvmasoiidjfvoasijdgpoviasjfdoivamsdfasaasdfvmas[jdfivjasopd
+a[s0dfjvpamosdjfpoivajsdpoifjvasdoifjvakjsnkjvqwrat
+awrothvoauwhrtouqvhwoautoauretivairtiveabritbveiartbva]rtuvah
+artvoaejrbtovaebortbv
+araeurtvaouerbt
+varutbv
+qertbv
+aertvaertvae
+rtvaert
+vartaberybwaerstynwr
+stynwsrtyneryune
+ rynwe
+ tyiwhtibyhw
+ eirthvq
+ erithv
+ q"""
+write_text("text.txt", s)
